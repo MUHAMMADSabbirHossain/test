ffb7dfed3a8dda67a466e3203b56d145f6ad869f
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-10-08 14:19:22 +0600

    Files folder directory has been changed.

---
+++ docker-compose-mern/express-docker/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.json({ message: "Hello World! Welcome to Express Docker." });
+});
+
+app.listen(5000, () => console.log("🤖 Server started on port 5000."));
